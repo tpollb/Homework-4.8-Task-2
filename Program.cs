f37a0559d5a273975e41a76a13615f0f70ddcd2a
Сергей Усков <q3mydoom@hotmail.com>
2021-10-28 13:25:09 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Homework_4._8_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayMaxValue;
+            int number;
+            int i;
+            int result;
+            
+            Console.WriteLine("Enter array lenght 1 to 255: ");
+            arrayMaxValue = int.Parse(Console.ReadLine());
+
+            int[] array = new int[arrayMaxValue];
+
+            Console.WriteLine($"Enter {arrayMaxValue} nubers");
+            //заполнение массива
+            for (i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"Number {i + 1}: ");
+                    number = int.Parse(Console.ReadLine());
+                    array[i] = number;
+                }
+
+            //вывод массива
+            /*
+            for (i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"Array: {array[i]} ");
+                }
+                Console.WriteLine();
+            */
+
+            //обработка. поиск наименьшего
+            Array.Sort(array);
+            result = array[0];
+            Console.WriteLine($"Min number is: {result}");
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
